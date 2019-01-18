fbf408946f7cdded3234add458b98e91a6245f3a
nik-rimor <nickkekes@gmail.com>
2019-01-18 20:09:25 +0200

    add user addition funcitonality to client

--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const UsersList = (props) => {
+
 	return (
 		<div>
 			{
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 // new
@@ -10,10 +11,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 	};
-	
+  
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => { 
+      console.log(res);
+      this.get_users();
+      this.setState({ username: '', email: '', }); 
+    })
+    .catch((err) => { console.log(err); });
+  };
+  
+  handleChange(event) {
+    const obj ={};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
   componentDidMount() {
     this.get_users();
   };
@@ -24,15 +50,21 @@ class App extends Component {
 		.catch((err) => { console.log(err); });
   };
   
-
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br/>
               <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+              <AddUser 
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <hr/><br/>
               <UsersList users={this.state.users} />
             </div>
