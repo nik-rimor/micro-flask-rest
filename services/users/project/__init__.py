9cd47a5d2c839a6fa4186c6d9f9f1f2129bea83e
nik-rimor <nickkekes@gmail.com>
2019-01-18 13:54:00 +0200

    add react client

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,12 +3,16 @@
 
 import os
 
+from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 
 # instantiate the db
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -16,12 +20,16 @@ def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
+    # enable CORS
+    CORS(app)
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # Check proper config is loaded
     # import sys
