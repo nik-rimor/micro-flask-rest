97ad749012bf7b551c32077e18ef58ebc9fce93b
nik-rimor <nickkekes@gmail.com>
2019-01-20 19:14:31 +0200

    jwt token supprt in model

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -18,13 +18,16 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
         self.assertFalse(current_app is None)
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
         self.assertTrue(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 class TestTestingConfig(TestCase):
@@ -33,7 +36,8 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
@@ -41,6 +45,8 @@ class TestTestingConfig(TestCase):
             os.environ.get('DATABASE_TEST_URL')
         )
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 0)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 3)
 
 
 class TestProductionConfig(TestCase):
@@ -49,6 +55,9 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
         self.assertFalse(app.config['TESTING'])
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
